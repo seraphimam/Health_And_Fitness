11e358a6a15b7e3dea79bb03cdad030f5faeb639
squidtempura <d17129502@mytudublin.ie>
2021-04-22 18:49:33 +0100

    connect healthyfood class, recipe class and exercise_video class to the database

--- app/src/main/java/com/example/health_and_fitness/DatabaseManager.java
+++ app/src/main/java/com/example/health_and_fitness/DatabaseManager.java
@@ -7,9 +7,12 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 
-
+import static com.example.health_and_fitness.DatabaseHelper.DATABASE_TABLE1;
+import static com.example.health_and_fitness.DatabaseHelper.DATABASE_TABLE2;
 import static com.example.health_and_fitness.DatabaseHelper.DATABASE_TABLE4;
+import static com.example.health_and_fitness.DatabaseHelper.DATABASE_TABLE5;
 import static com.example.health_and_fitness.DatabaseHelper.DATABASE_TABLE6;
+import static com.example.health_and_fitness.DatabaseHelper.DATABASE_TABLE7;
 
 import static com.example.health_and_fitness.DatabaseHelper.KEY_FRIENDS_ID;
 import static com.example.health_and_fitness.DatabaseHelper.KEY_FIENDS_NAME;
@@ -21,6 +24,12 @@ import static com.example.health_and_fitness.DatabaseHelper.KEY_WALK_STEPS;
 import static com.example.health_and_fitness.DatabaseHelper.KEY_FOOD_ID;
 import static com.example.health_and_fitness.DatabaseHelper.KEY_FOOD_NAME;
 
+import static com.example.health_and_fitness.DatabaseHelper.KEY_EXERCISE_ID;
+import static com.example.health_and_fitness.DatabaseHelper.KEY_EXERCISE_NAME;
+import static com.example.health_and_fitness.DatabaseHelper.KEY_EXERCISE_VIDEO_URL;
+
+import static com.example.health_and_fitness.DatabaseHelper.KEY_RECIPE_ID;
+import static com.example.health_and_fitness.DatabaseHelper.KEY_RECIPE_VIDEO_VID;
 
 import static com.example.health_and_fitness.DatabaseHelper.KEY_FOOD_INFORMATION_ID;
 import static com.example.health_and_fitness.DatabaseHelper.KEY_FOOD_INFORMATION_NAME;
@@ -74,6 +83,23 @@ public class DatabaseManager extends MainActivity
         return myDatabase.insert(DATABASE_TABLE6, null, initialValues);
     }
 
+    public long insertRercipeVideo(int rowId, String videoVid)
+    {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_RECIPE_ID,rowId);
+        initialValues.put(KEY_RECIPE_VIDEO_VID,videoVid);
+        return myDatabase.insert(DATABASE_TABLE7, null, initialValues);
+    }
+
+    public long insertExerciseVideo(int rowId, String videoVid)
+    {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_EXERCISE_ID,rowId);
+        initialValues.put(KEY_EXERCISE_NAME,videoVid);
+        initialValues.put(KEY_EXERCISE_VIDEO_URL,videoVid);
+        return myDatabase.insert(DATABASE_TABLE5, null, initialValues);
+    }
+
     public Cursor SearchFoodInformation(int rowId, String foodName) throws SQLException
     {
         Cursor mCursor = null;
@@ -112,6 +138,69 @@ public class DatabaseManager extends MainActivity
             mCursor.moveToFirst();
         }
         return mCursor;
+    }
+
+    public Cursor SearchRecipeVideo(int rowId) throws SQLException
+    {
+        Cursor mCursor = null;
+
+            mCursor = myDatabase.query(true, DATABASE_TABLE7, new String[] {
+                            KEY_RECIPE_ID,
+                            KEY_RECIPE_VIDEO_VID
+                    },
+                    KEY_RECIPE_ID + " = " + rowId,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null);
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    public long getRecipeVideoAmount()
+    {
+        String sql = "select count(*) FROM " + DATABASE_TABLE7;
+        Cursor cursor = myDatabase.rawQuery(sql, null);
+        cursor.moveToFirst();
+        long count = cursor.getLong(0);
+        cursor.close();
+        return count;
+    }
+
+    public Cursor SearchExerciseVideo(int rowId) throws SQLException
+    {
+        Cursor mCursor = null;
+
+        mCursor = myDatabase.query(true, DATABASE_TABLE5, new String[] {
+                        KEY_EXERCISE_ID,
+                        KEY_EXERCISE_NAME,
+                        KEY_EXERCISE_VIDEO_URL
+                },
+                KEY_EXERCISE_ID + " == " + "\'" + rowId + "\'",
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    public long getExerciseVideoAmount()
+    {
+        String sql = "select count(*) FROM " + DATABASE_TABLE5;
+        Cursor cursor = myDatabase.rawQuery(sql, null);
+        cursor.moveToFirst();
+        long count = cursor.getLong(0);
+        cursor.close();
+        return count;
     }
 
     public Cursor getAllFood() {
--- app/src/main/java/com/example/health_and_fitness/Recipe.java
+++ app/src/main/java/com/example/health_and_fitness/Recipe.java
@@ -1,5 +1,6 @@
 package com.example.health_and_fitness;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -11,29 +12,22 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Recipe extends AppCompatActivity {
     private int vid = 0;
     static String url_loc = "https://vimeo.com/showcase/8125021/video/";
-
-    String[] video = {"511582457", "509716824", "508037594", "507084029",
-            "506147217", "506097066", "505739508", "505739133", "502649719",
-             "501905233", "499200488", "499125231", "497625898", "492401376"
-            , "492399739", "492399293", "488011559", "488010676", "485470550"
-            , "484449058", "483962914", "483468322", "481160577", "480462567"
-            , "480272376", "478385295", "477151725", "477151520", "475913883"
-            , "474654482", "470585451", "468475783", "463408140", "460959448"
-            , "460191457", "460191299", "458576336", "458576018", "457061568"
-            , "456265984", "455223106", "455217677", "455217266", "451244111"
-            , "449336677", "441069890", "439968079", "438536665", "437842896"
-            , "436767300", "431459085", "430704167", "430343325", "429933575"
-            , "429933554"};
+    DatabaseManager dbm = new DatabaseManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe);
 
-        String url = url_loc + video[vid] + "/embed";
-
         WebView w = (WebView) findViewById(R.id.exercise_video);
         w.getSettings().setJavaScriptEnabled(true);
+
+        Cursor mCursor;
+        final DatabaseManager dbm = new DatabaseManager(this);
+        dbm.open();
+        mCursor = dbm.SearchRecipeVideo(vid);
+        String url = url_loc + mCursor.getString(1)+ "/embed";
+
         w.loadUrl(url);
 
         Button up = (Button) findViewById(R.id.up);
@@ -54,7 +48,13 @@ public class Recipe extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 vid--;
-                String prev_url = url_loc + video[vid] + "/embed";
+
+                Cursor mCursor;
+
+                dbm.open();
+                mCursor = dbm.SearchRecipeVideo(vid);
+                String prev_url = url_loc + mCursor.getString(1)+ "/embed";
+
                 WebView w = (WebView) findViewById(R.id.exercise_video);
                 w.getSettings().setJavaScriptEnabled(true);
                 w.loadUrl(prev_url);
@@ -66,7 +66,13 @@ public class Recipe extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 vid++;
-                String next_url = url_loc + video[vid] + "/embed";
+
+                Cursor mCursor;
+
+                dbm.open();
+                mCursor = dbm.SearchRecipeVideo(vid);
+                String next_url = url_loc + mCursor.getString(1)+ "/embed";
+
                 WebView w = (WebView) findViewById(R.id.exercise_video);
                 w.getSettings().setJavaScriptEnabled(true);
                 w.loadUrl(next_url);
@@ -80,6 +86,9 @@ public class Recipe extends AppCompatActivity {
         Button up = (Button) findViewById(R.id.up);
         Button down = (Button) findViewById(R.id.down);
 
+        dbm.open();
+        long amount = dbm.getRecipeVideoAmount();
+
         if(vid == 0){
             up.setVisibility(View.INVISIBLE);
             up.setClickable(false);
@@ -88,7 +97,7 @@ public class Recipe extends AppCompatActivity {
             up.setClickable(true);
         }
 
-        if(vid == (video.length - 1)){
+        if(vid == (amount - 1)){
             down.setVisibility(View.INVISIBLE);
             down.setClickable(false);
         }else{
